923db1217fe4ffa447f8a945303dc1dfdb04c9c6
Dajana Bulic <bulic.dajanaaa@gmail.com>
2026-07-04 16:36:04 +0200

    Add GetFullPlanAsync to TripClient for VIEW share resolution

--- backend/TravelPlanner/SharingService/Services/TripClient.cs
+++ backend/TravelPlanner/SharingService/Services/TripClient.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 
@@ -25,6 +26,25 @@ namespace SharingService.Services
 
             var response = await _httpClient.SendAsync(request);
             return response.IsSuccessStatusCode;
+        }
+
+        public async Task<JsonElement?> GetFullPlanAsync(Guid tripId)
+        {
+            var tripServiceUrl = _configuration["Services:TripServiceUrl"];
+            var internalKey = _configuration["Internal:ApiKey"];
+
+            var request = new HttpRequestMessage(HttpMethod.Get, $"{tripServiceUrl}/api/internal/trips/{tripId}/full");
+            request.Headers.Add("X-Internal-Key", internalKey);
+
+            var response = await _httpClient.SendAsync(request);
+            if (!response.IsSuccessStatusCode)
+            {
+                return null;
+            }
+
+            using var stream = await response.Content.ReadAsStreamAsync();
+            var document = await JsonDocument.ParseAsync(stream);
+            return document.RootElement.Clone();
         }
     }
 }

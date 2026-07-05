541a7961d2699d6cb16e8ee040521921c6e4514c
Dajana Bulic <bulic.dajanaaa@gmail.com>
2026-07-06 01:20:29 +0200

    Add Init migration for Beleske and Podsetnici tables

--- backend/TravelPlanner/TripService/Migrations/20260705231849_AddBeleskeAndPodsetnici.Designer.cs
+++ backend/TravelPlanner/TripService/Migrations/20260705231849_AddBeleskeAndPodsetnici.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TripService.Data;
 
@@ -11,9 +12,11 @@ using TripService.Data;
 namespace TripService.Migrations
 {
     [DbContext(typeof(TripDbContext))]
-    partial class TripDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260705231849_AddBeleskeAndPodsetnici")]
+    partial class AddBeleskeAndPodsetnici
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/TravelPlanner/TripService/Migrations/20260705231849_AddBeleskeAndPodsetnici.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TripService.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBeleskeAndPodsetnici : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Beleske",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PlanId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Naslov = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Sadrzaj = table.Column<string>(type: "nvarchar(max)", maxLength: 5000, nullable: false),
+                    DatumKreiranja = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Beleske", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Beleske_Plans_PlanId",
+                        column: x => x.PlanId,
+                        principalTable: "Plans",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Podsetnici",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PlanId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Naziv = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Datum = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Opis = table.Column<string>(type: "nvarchar(2000)", maxLength: 2000, nullable: true),
+                    Zavrseno = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Podsetnici", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Podsetnici_Plans_PlanId",
+                        column: x => x.PlanId,
+                        principalTable: "Plans",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Beleske_PlanId",
+                table: "Beleske",
+                column: "PlanId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Podsetnici_PlanId",
+                table: "Podsetnici",
+                column: "PlanId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Beleske");
+
+            migrationBuilder.DropTable(
+                name: "Podsetnici");
+        }
+    }
+}
